1a24b122313d09815a893cc1297da813d9e37aeb
@varis.dogukan <dogukan.varis@glomil.com>
2025-05-24 07:54:46 +0300

    Added Heading and basic component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,21 @@
+import PromptForm from "@/components/PromptForm";
+import ResultCard from "@/components/ResultCard";
+
 export default function HomePage() {
-  return <>Promptly</>;
+  return (
+    <main className='min-h-screen bg-slate-900 text-slate-400'>
+      <div className='max-w-7xl mx-auto '>
+        <div className='text-center pt-8 h-screen flex flex-col items-center justify-center'>
+          <h1 className='text-9xl font-bold mb-6'>Promptly</h1>
+          <p className='text-3xl text-slate-600 mb-20'>
+            AI destekli içerik üretici – fikirlerini birkaç saniyede yazıya dök.
+          </p>
+
+          <PromptForm />
+        </div>
+
+        <ResultCard />
+      </div>
+    </main>
+  );
 }
---
+++ src/components/PromptForm.tsx
@@ -0,0 +1,3 @@
+export default function PromptForm() {
+  return <div>PromptForm</div>;
+}
