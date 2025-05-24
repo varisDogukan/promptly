4dc89f1434ac93cf6022f7975955c66cbabca26c
@varis.dogukan <dogukan.varis@glomil.com>
2025-05-24 08:48:22 +0300

    Updated PromptForm

--- src/components/PromptForm.tsx
+++ src/components/PromptForm.tsx
@@ -1,3 +1,19 @@
 export default function PromptForm() {
-  return <div>PromptForm</div>;
+  return (
+    <form className='max-w-[400] w-full'>
+      <input
+        type='text'
+        id='inputPrompt'
+        name='inputPrompt'
+        placeholder='Ne hakkında yazmamı istersin?'
+        className='p-4 bg-slate-950 rounded-lg w-full  border border-slate-800 placeholder:opacity-50 mb-6 '
+      />
+      <button
+        type='submit'
+        className='text-slate-500 hover:text-slate-400 px-4 py-2 rounded-lg font-medium duration-200 ease-linear'
+      >
+        Oluştur
+      </button>
+    </form>
+  );
 }
